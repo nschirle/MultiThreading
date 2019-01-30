7132f6e8018fe639bd4ef0a6ca59535dbe813189
Nathan Schirle <schirle.nathan@gmail.com>
2019-01-30 09:19:17 -0800

    Add project files.

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ThreadingThruTask()
+        {
+            var t1 = new Task(() =>
+            {
+                Console.WriteLine("task 1");
+            });
+            t1.Start();
+        }
+    }
+
+}
